ca1f2d8592790ac9069c5f0d10075d269cb66330
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2025-02-08 16:10:54 +0700

    add game mode screen

--- src/pages/GameMode/GameMode.js
+++ src/pages/GameMode/GameMode.js
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
+import './GameMode.css'
+
 export const GameModePage = () => {
   return (
-    <div>GameModepage</div>
+    <div className="gamemode">
+      <div className='gamemode-selector'>
+        <Link className='gamemode-left' to='/guess'>
+          <p className='gamemode-text'>Guest<br/>Movie</p>
+        </Link>
+        <Link className='gamemode-right' to='/suggest'>
+          <p className='gamemode-text'>Suggest<br/>Movie</p>
+        </Link>
+      </div>
+    </div>
   )
 }
